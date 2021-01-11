0af39adbdaefd07b8d57106b18f183527823a57f
Yunusxoja <yunus1616@mail.ru>
2021-01-11 18:42:47 +0500

    Первый коммит проекта: 1) Полностью готов первый до 2) Есть меню 3) Добавил логотип

---
+++ Assets/Scripts/EnterScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnterScript : MonoBehaviour
+{
+    public UnityEvent OnEnter;
+    public UnityEvent OnExit;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player")
+        {
+            OnEnter.Invoke();
+        }
+        
+    }
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player")
+        {
+            OnExit.Invoke();
+        }
+        
+    }
+}
