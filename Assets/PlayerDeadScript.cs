d022737f29288620e12c1ff52deb5f79a5c1760f
Yunusxoja <yunus1616@mail.ru>
2021-06-03 18:34:51 +0500

    Сделал систему смерти

---
+++ Assets/PlayerDeadScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PlayerDeadScript : MonoBehaviour
+{
+    public UnityEvent DeadEvent;
+    public UnityEvent RespEvent;
+    Transform PlayerTrans;
+    public GameObject Camera;
+    public GameObject Canvas;
+    Animator anim;
+    public Transform checkPoint;
+    public Transform CameraPosition;
+    void Start() {
+        PlayerTrans = this.GetComponent<Transform>();
+        anim = Camera.GetComponent<Animator>();
+    }
+    public void SetCheckPoint(Transform Point)
+    {
+        checkPoint = Point;
+    }
+    public void Dead()
+    {
+        DeadEvent.Invoke();
+        anim.Play("CameraDeath");
+        
+        Canvas.GetComponent<Animator>().Play("CanvasEnable");
+    }
+    public void reSpawn()
+    {
+        PlayerTrans.position = checkPoint.position;
+        Camera.transform.position = CameraPosition.position;
+    }
+
+
+}
